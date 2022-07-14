040029238d9e682017eaad4b07ac1f869ab634a0
jecko85 <102580302+jecko85@users.noreply.github.com>
2022-07-14 09:50:49 +0200

    primo esercizio

---
+++ esercizio.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+	int numero1;
+	int somma;
+	int somma1;
+      
+	printf("Inserisci numero \n");
+	scanf("%d" , & numero1);
+
+        somma= numero1 +1;
+	printf("Numero incrementato: %d\n", somma);
+
+	somma1= numero1 -1;
+	printf("Numero decrementato: %d\n" , somma1);	
+
+      	
+
+return 0;
+
+	
+}
